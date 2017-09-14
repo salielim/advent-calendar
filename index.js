ff2858d7b589550c4afcec9182321d2da2ddd8f6
Salie Lim <salie.lim@gmail.com>
2017-09-14 19:53:42 +0800

    Fill images of box grid based on today's date

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-var todaysDate = new Date();
-console.log(todaysDate);
+const todaysDate = new Date();
+const todaysDay = todaysDate.getDate();
 
-var todaysDay = todaysDate.getDate();
-console.log(todaysDay);
+const boxGrid = document.getElementById('box-grid');
+const numberToWords = {
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen'
+};
+let newHtml = ''
+
+for (let i = 1; i < 17; i++) {
+  if (todaysDay > i) {
+    newHtml += `<div class="${ numberToWords[i]} gift-box lipstick-hide"></div>`
+  } else if (todaysDay === i) {
+    newHtml += `<div id="todays-box" class="${ numberToWords[i]} gift-box snowflakes numbers">${ i }</div>`
+  } else {
+    newHtml += `<div class="${ numberToWords[i]} gift-box numbers">${ i }</div>`
+  }
+};
+
+boxGrid.insertAdjacentHTML('afterbegin', newHtml);
+const special = document.getElementById('todays-box');
+
+special.onclick = (event) => {
+    const specialDiv = event.target
+    specialDiv.classList.toggle('numbers');
+    specialDiv.classList.toggle('hide-number');
+    specialDiv.classList.toggle('snowflakes');
+    specialDiv.classList.toggle('lipstick-show');
+  }
